5bcfb5539c99a0182fa28d3c51a15362339fe908
Matt Nadar <matthewnadar4@gmail.com>
2023-08-10 12:21:12 -0400

    fixed progress bar being wonky and not filling all the way

--- scripts.js
+++ scripts.js
@@ -6,13 +6,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let progress = 0;
     const interval = setInterval(() => {
-      if (progress >= 100) {
+      if (progress >= 124) {
         clearInterval(interval);
         overlay.style.display = "none";
         startSection.style.display = "block";
         document.body.style.overflow = "auto";
       } else {
-        progress += 1.5;
+        progress += 1;
         loadingProgress.style.width = `${progress}%`;
       }
     }, 50);
