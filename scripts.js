79125d74edad044df8607fa172aab346103824eb
Matthew Nadar <matthewnadar4@gmail.com>
2023-08-10 11:55:02 -0400

    added loading page thing

---
+++ scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const overlay = document.querySelector(".overlay");
+    const startSection = document.querySelector("#start");
+    const loadingProgress = document.querySelector(".loading-progress");
+    document.body.style.overflow = "hidden";
+
+    let progress = 0;
+    const interval = setInterval(() => {
+      if (progress >= 100) {
+        clearInterval(interval);
+        overlay.style.display = "none";
+        startSection.style.display = "block";
+        document.body.style.overflow = "auto";
+      } else {
+        progress += 1.5;
+        loadingProgress.style.width = `${progress}%`;
+      }
+    }, 50);
+  });
