7511dfe5273891f005c72ac6a2024542994b4d4b
Matthew Nadar <matthewnadar4@gmail.com>
2023-09-03 10:53:52 -0400

    Loading bar fixes

--- scripts.js
+++ scripts.js
@@ -6,14 +6,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let progress = 0;
     const interval = setInterval(() => {
-      if (progress >= 124) {
+      if (progress >= 175) {
         clearInterval(interval);
         overlay.style.display = "none";
         startSection.style.display = "block";
         document.body.style.overflow = "auto";
       } else {
-        progress += 1;
-        loadingProgress.style.width = `${progress}%`;
+        progress += 5;
+        loadingProgress.style.width = `${progress}%`; 
       }
     }, 50);
   });
